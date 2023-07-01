825ef67a75e3c2a2b26278f907efdfbe132a2f33
Alexey Sevastynov <alexseva94@gmail.com>
2023-07-01 20:31:36 +0300

    included cluster Atlas mongoDB

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 // 1. npm init
 // 2. npm i expess
 // 3. npm i nodemon, + "scripts" : {..., "dev": "nodemon index.js" } . npm run dev
+// 4. npm i mongoose
+// 5. npm i dotenv . Hide password the cluster, mongodb
 
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("DB OK!"))
+  .catch((err) => console.log("DB error:", err));
 
 const app = express();
 const PORT = 9999;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send(`Hello Woorld`);
